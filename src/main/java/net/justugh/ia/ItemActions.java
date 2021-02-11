614cc5b9c78aaabf3f39a17584bc3941a206dfa3
Justin <33465177+Justugh@users.noreply.github.com>
2021-02-11 09:11:17 -0500

    Remove Aikar Command System & Increase Version

--- src/main/java/net/justugh/ia/ItemActions.java
+++ src/main/java/net/justugh/ia/ItemActions.java
@@ -1,14 +1,10 @@
 package net.justugh.ia;
 
-import co.aikar.commands.PaperCommandManager;
-import com.google.common.collect.Lists;
 import lombok.Getter;
 import net.justugh.ia.command.ItemActionsCommand;
 import net.justugh.ia.item.ItemManager;
 import net.justugh.ia.listener.ItemActionListener;
 import org.bukkit.plugin.java.JavaPlugin;
-
-import java.util.List;
 
 @Getter
 public class ItemActions extends JavaPlugin {
@@ -16,7 +12,6 @@ public class ItemActions extends JavaPlugin {
     @Getter
     private static ItemActions instance;
 
-    private PaperCommandManager commandManager;
     private ItemManager itemManager;
 
     @Override
@@ -24,10 +19,12 @@ public class ItemActions extends JavaPlugin {
         instance = this;
 
         saveDefaultConfig();
-        commandManager = new PaperCommandManager(this);
         itemManager = new ItemManager(this);
 
-        loadCommands();
+        ItemActionsCommand command = new ItemActionsCommand(this);
+
+        getCommand("itemactions").setExecutor(command);
+        getCommand("itemactions").setTabCompleter(command);
 
         getServer().getPluginManager().registerEvents(new ItemActionListener(itemManager), this);
     }
@@ -35,16 +32,6 @@ public class ItemActions extends JavaPlugin {
     @Override
     public void onDisable() {
 
-    }
-
-    private void loadCommands() {
-        commandManager.getCommandCompletions().registerCompletion("ItemIds", context -> {
-            List<String> items = Lists.newArrayList();
-            itemManager.getItems().forEach(itemId -> items.add(itemId.getId()));
-            return items;
-        });
-
-        commandManager.registerCommand(new ItemActionsCommand());
     }
 
 }
--- src/main/java/net/justugh/ia/command/ItemActionsCommand.java
+++ src/main/java/net/justugh/ia/command/ItemActionsCommand.java
@@ -1,53 +1,95 @@
 package net.justugh.ia.command;
 
-import co.aikar.commands.BaseCommand;
-import co.aikar.commands.annotation.*;
+import com.google.common.collect.Lists;
 import net.justugh.ia.ItemActions;
 import org.bukkit.Bukkit;
+import org.bukkit.command.Command;
+import org.bukkit.command.CommandExecutor;
 import org.bukkit.command.CommandSender;
+import org.bukkit.command.TabCompleter;
 import org.bukkit.entity.Player;
 
-@CommandAlias("itemactions|ia")
-public class ItemActionsCommand extends BaseCommand {
+import java.util.List;
 
-    @Dependency
-    private ItemActions plugin;
+public class ItemActionsCommand implements CommandExecutor, TabCompleter {
 
-    @Default
-    @CommandPermission("itemactions.help")
-    public void onDefault(CommandSender commandSender) {
-        commandSender.sendMessage("§aItem Actions Commands:");
-        commandSender.sendMessage("§a/ia reload §8- §eReload the current items and configuration.");
-        commandSender.sendMessage("§a/ia give <player> <id> §8- §eGive a player an action item.");
+    private final ItemActions plugin;
+
+    public ItemActionsCommand(ItemActions plugin) {
+        this.plugin = plugin;
     }
 
-    @Subcommand("reload")
-    @CommandPermission("itemactions.reload")
-    public void onReload(CommandSender sender) {
-        plugin.reloadConfig();
-        plugin.getItemManager().loadItems(plugin.getConfig());
-        sender.sendMessage("§aSuccessfully reloaded the configuration and loaded §e" + plugin.getItemManager().getItems().size() + " §aitems.");
+    @Override
+    public boolean onCommand(CommandSender sender, Command command, String label, String[] args) {
+        if (args.length == 0) {
+            if (!sender.hasPermission("itemactions.help")) {
+                sender.sendMessage("§cNo permission.");
+                return true;
+            }
+
+            sender.sendMessage("§aItem Actions Commands:");
+            sender.sendMessage("§a/ia reload §8- §eReload the current items and configuration.");
+            sender.sendMessage("§a/ia give <player> <id> §8- §eGive a player an action item.");
+            return true;
+        }
+
+        if (args[0].equalsIgnoreCase("reload")) {
+            if (!sender.hasPermission("itemactions.reload")) {
+                sender.sendMessage("§cNo permission.");
+                return true;
+            }
+
+            plugin.reloadConfig();
+            plugin.getItemManager().loadItems(plugin.getConfig());
+            sender.sendMessage("§aSuccessfully reloaded the configuration and loaded §e" + plugin.getItemManager().getItems().size() + " §aitems.");
+            return true;
+        }
+
+        if (args[0].equalsIgnoreCase("give")) {
+            if (!sender.hasPermission("itemactions.give")) {
+                sender.sendMessage("§cNo permission.");
+                return true;
+            }
+
+            if (args.length < 3) {
+                sender.sendMessage("§cUsage: /ia give <player> <id>");
+                return true;
+            }
+
+            Player target = Bukkit.getPlayer(args[1]);
+
+            if (target == null) {
+                sender.sendMessage("§cInvalid player.");
+                return true;
+            }
+
+            if (plugin.getItemManager().getItem(args[2]) == null) {
+                sender.sendMessage("§cInvalid Action Item.");
+                return true;
+            }
+
+            plugin.getItemManager().getItem(args[2]).getData().getItems().forEach(item -> target.getInventory().addItem(item));
+            sender.sendMessage("§aSuccessfully gave §e" + target.getName() + " §athe §e" + args[2] + " §aAction Item(s)!");
+        }
+
+        return true;
     }
 
-    @Subcommand("give")
-    @CommandPermission("itemactions.give")
-    @Syntax("<player> <id>")
-    @CommandCompletion("@Players @ItemIds")
-    public void onGive(CommandSender sender, String player, String id) {
-        Player target = Bukkit.getPlayer(player);
-
-        if (target == null) {
-            sender.sendMessage("§cInvalid player.");
-            return;
+    @Override
+    public List<String> onTabComplete(CommandSender sender, Command command, String alias, String[] args) {
+        if (!args[0].equalsIgnoreCase("give")) {
+            return null;
         }
 
-        if (plugin.getItemManager().getItem(id) == null) {
-            sender.sendMessage("§cInvalid Action Item.");
-            return;
+        if (args.length == 2) {
+            List<String> onlinePlayers = Lists.newArrayList();
+            Bukkit.getOnlinePlayers().forEach(player -> onlinePlayers.add(player.getName()));
+            return onlinePlayers;
         }
 
-        plugin.getItemManager().getItem(id).getData().getItems().forEach(item -> target.getInventory().addItem(item));
-        sender.sendMessage("§aSuccessfully gave §e" + target.getName() + " §athe §e" + id + " §aAction Item(s)!");
+        List<String> items = Lists.newArrayList();
+        plugin.getItemManager().getItems().forEach(itemId -> items.add(itemId.getId()));
+        return items;
     }
 
 }
